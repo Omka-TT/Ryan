f9412ca8d841af9bca16638ffb2a0e7492354439
Omka <alphhalife@gmail.com>
2024-10-31 19:15:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Остановить отправку формы
+    alert('Спасибо за ваше сообщение!'); // Вывод сообщения
+});
