e38b1761a038d21dc1ba7b67a64473dfd0006cbc
Omka <alphhalife@gmail.com>
2024-12-13 12:03:35 +0600

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Остановить отправку формы
-    alert('Спасибо за ваше сообщение!'); // Вывод сообщения
-});
